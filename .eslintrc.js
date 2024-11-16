ca1d0e4a64d9114e8be760fdcb5989ab74319728
Vitalii Melnychuk <vvv.melnychuk@gmail.com>
2024-11-16 00:30:04 +0000

    feat: ci action

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,31 @@
 module.exports = {
-  root: true,
-  extends: ['@workalizer/eslint-config']
+  parser: "@typescript-eslint/parser",
+  plugins: ["prettier", "simple-import-sort", "@typescript-eslint"],
+  extends: ["prettier", "plugin:@typescript-eslint/recommended"],
+  rules: {
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": ["error"],
+    "@typescript-eslint/no-namespace": "off",
+    "prettier/prettier": "error",
+    "no-console": 1,
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
+    // Code complexity rules
+    complexity: ["error", 20],
+    "max-depth": ["error", 5],
+    "max-nested-callbacks": ["error", 3],
+    "max-statements": ["error", 20],
+    "max-lines": ["error", 800],
+    "eol-last": ["error", "always"],
+    "no-restricted-imports": [
+      "error",
+      {
+        patterns: [".*"],
+      },
+    ],
+  },
+  env: {
+    node: true,
+    es2021: true,
+  },
 };
